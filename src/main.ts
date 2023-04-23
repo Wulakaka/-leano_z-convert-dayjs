3d1021360837b3826981546e39b2e04b34aadcfa
张伟峰 <zhang.weifeng@tslsmart.com>
2023-04-23 17:35:55 +0700

    feat: 完善测试文件

--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,32 @@
-import dayjs, { type ConfigType, type Dayjs } from 'dayjs'
-import utc from 'dayjs/plugin/utc'
-import timezone from 'dayjs/plugin/timezone'
-import customParseFormat from 'dayjs/plugin/customParseFormat'
+import dayjs, { type ConfigType, type Dayjs } from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+import customParseFormat from "dayjs/plugin/customParseFormat";
 
-dayjs.extend(utc)
-dayjs.extend(timezone)
-dayjs.extend(customParseFormat)
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.extend(customParseFormat);
 
 export function convertToLocal(
-    date: ConfigType,
-    format?: string
+  date: ConfigType,
+  format?: string
 ): Dayjs & {
-    server(): Dayjs
+  server(): Dayjs;
 } {
-    let instance = format ? dayjs.tz(date, format, 'Asia/Shanghai') : dayjs.tz(date, 'Asia/Shanghai')
-    instance = instance.local()
+  let instance = format
+    ? dayjs.tz(date, format, "Asia/Shanghai")
+    : dayjs.tz(date, "Asia/Shanghai");
+  instance = instance.local();
 
-    return Object.assign(instance, {
-        server() {
-            return instance.tz('Asia/Shanghai')
-        },
-    })
+  return Object.assign(instance, {
+    server() {
+      return instance.tz("Asia/Shanghai");
+    },
+  });
 }
 
-export function convertToServer(
-    date: ConfigType,
-    format?: string
-): Dayjs {
-    let instance = dayjs(date, format)
-    instance = instance.tz('Asia/Shanghai')
-    return instance
+export function convertToServer(date: ConfigType, format?: string): Dayjs {
+  let instance = dayjs(date, format);
+  instance = instance.tz("Asia/Shanghai");
+  return instance;
 }
-
-// 获取当地时间表示
-// console.log(convertDayjs('2023-04-23').format())
-// 获取服务端需要的时间表示
-// console.log(convertDayjs('2023-04-23').serverTime().format())
---
+++ test/main.test.ts
@@ -0,0 +1,21 @@
+import {describe, expect, test} from 'vitest'
+import {convertToLocal, convertToServer} from "@/main";
+import dayjs from "dayjs";
+
+describe('convertToLocal',() => {
+    test('将服务器传回的东八区时间转换为当地时间', () => {
+        expect(convertToLocal('2023/4/23 16:28:00').format('HH')).toBe('15')
+    })
+    test('服务器返回的东八区时间经过转换后仍然为东八区时间',() => {
+        expect(convertToLocal('2023/4/23 16:28:00').server().format('HH')).toBe('16')
+    })
+    test('带有时区信息的时间不受影响',() => {
+        expect(convertToLocal(new Date()).format('HH')).toEqual(dayjs().format('HH'))
+    })
+})
+
+describe("convertToServer", () => {
+    test("将当地时间转换为东八区时间",() => {
+        expect(convertToServer(new Date()).format('HH')).toEqual('18')
+    })
+})
